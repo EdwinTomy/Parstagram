02dfcdf753001cd54e9df8dd105275e4de7acfdd
Edwin Tomy George <edwintg@fb.com>
2020-07-06 18:04:22 -0600

    Define Post Object and Query

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -9,17 +9,61 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity{
+
+    public static final String TAG = "MainActivity";
+
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Linking elements between java and xml
+        etDescription = findViewById(R.id.etDescription);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        //Retrieving ParseObject
+        queryPosts();
+    }
+
+    //Retrieving ParseObject
+    private void queryPosts() {
+
+        //Object to be queried (Post)
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+
+        //Object id of Post
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                // Access data using the getter methods for the object
+                for(Post post : posts){
+                    Log.i(TAG, "Post: " + post.getDescription());
+                }
+            }
+        });
     }
 }
